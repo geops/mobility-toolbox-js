3104629b98032e57f20f72ac1665521ec161b84d
oterral <oterral@users.noreply.github.com>
2020-07-13 17:48:10 +0200

    Ensure rotation is et to 0

--- src/ol/layers/MapboxLayer.js
+++ src/ol/layers/MapboxLayer.js
@@ -59,7 +59,7 @@ export default class MapboxLayer extends Layer {
         // adjust view parameters in mapbox
         const { rotation } = viewState;
         if (rotation && this.renderState.rotation !== rotation) {
-          this.mbMap.rotateTo((-rotation * 180) / Math.PI, {
+          this.mbMap.rotateTo((-(rotation || 0) * 180) / Math.PI, {
             animate: false,
           });
           changed = true;
