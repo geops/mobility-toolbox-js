a4aa5f3c87e061c1abbc877df2282ec13e7fa319
Mario Härtwig <mario.haertwig@geops.de>
2020-10-06 18:08:20 +0200

    Add a quick start guide

---
+++ src/doc/components/TrackerExample.js
@@ -0,0 +1,39 @@
+import React, { useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Map } from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import { TrajservLayer } from '../../mapbox';
+
+const useStyles = makeStyles({
+  root: {
+    width: '100%',
+    height: 300,
+    pointerEvents: 'none',
+  },
+});
+
+const TrackerExample = () => {
+  const classes = useStyles();
+
+  useEffect(() => {
+    const map = new Map({
+      container: 'map',
+      style: `https://maps.geops.io/styles/travic/style.json?key=${window.apiKey}`,
+      center: [7.4707, 46.95],
+      zoom: 12,
+    });
+
+    const tracker = new TrajservLayer({
+      url: 'https://api.geops.io/tracker/v1',
+      apiKey: window.apiKey,
+    });
+
+    map.on('load', () => {
+      tracker.init(map, 'waterway-name');
+    });
+  }, []);
+
+  return <div id="map" className={classes.root} />;
+};
+
+export default TrackerExample;
