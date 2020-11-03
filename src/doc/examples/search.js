877531a0972c8aae3029b1999699f8c0f6d0de14
Mario Härtwig <mario.haertwig@geops.de>
2020-11-03 16:54:01 +0100

    Display search results in tag cloud

--- src/doc/examples/search.js
+++ src/doc/examples/search.js
@@ -1,47 +1,24 @@
-import View from 'ol/View';
-import Vectorlayer from 'ol/layer/Vector';
-import VectorSource from 'ol/source/Vector';
-import GeoJSON from 'ol/format/GeoJSON';
 import { StopsAPI } from '../../api';
-import { Map, MapboxLayer } from '../../ol';
 import 'ol/ol.css';
 
 export default () => {
   const api = new StopsAPI({
-    url: 'https://api.geops.io/stops/v1/',
+    params: { limit: 20 },
     apiKey: window.apiKey,
   });
 
-  const map = new Map({
-    target: 'map',
-    view: new View({
-      center: [831634, 5933959],
-      zoom: 8,
-    }),
-  });
+  const resultDiv = document.getElementById('results');
+  document.getElementById('search').onkeyup = (evt) => {
+    api.search({ limit: 30, q: evt.target.value }).then((data) => {
+      resultDiv.innerHTML = '';
 
-  const mapboxLayer = new MapboxLayer({
-    url: `https://maps.geops.io/styles/base_bright_v1/style.json?key=${window.apiKey}`,
-  });
-
-  const vectorSource = new VectorSource();
-  const vectorLayer = new Vectorlayer({ source: vectorSource });
-
-  map.addLayer(mapboxLayer);
-  map.addLayer(vectorLayer);
-
-  const input = document.getElementById('search');
-  input.onkeyup = (evt) => {
-    vectorSource.clear(true);
-
-    api.search({ q: evt.target.value }).then((featureCollection) => {
-      const features = new GeoJSON().readFeatures(featureCollection, {
-        featureProjection: map.getView().getProjection(),
-      });
-      vectorSource.addFeatures(features);
-
-      map.getView().fit(vectorSource.getExtent(), {
-        padding: [50, 50, 50, 50],
+      data.features.forEach(({ properties }) => {
+        const span = document.createElement('div');
+        // eslint-disable-next-line
+        span.style.color = `#${(((1 << 24) * Math.random()) | 0).toString(16)}`;
+        span.style.fontSize = `${100 * (1 - properties.rank)}%`;
+        span.innerText = properties.name;
+        resultDiv.appendChild(span);
       });
     });
   };
