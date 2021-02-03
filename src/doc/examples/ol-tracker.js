c9fb7d68efc6e30e10932cb1517a304363e25ab9
edellucien <edellucien@gmail.com>
2021-02-03 17:14:37 +0100

    Test commit to have a filterFunc use case

--- src/doc/examples/ol-tracker.js
+++ src/doc/examples/ol-tracker.js
@@ -1,4 +1,10 @@
 import View from 'ol/View';
+
+import Feature from 'ol/Feature';
+import Vector from 'ol/source/Vector';
+import VectorLayer from 'ol/layer/Vector';
+
+import { Polygon } from 'ol/geom';
 import { Map, TrajservLayer, MapboxLayer } from '../../ol';
 import 'ol/ol.css';
 import CopyrightControl from '../../ol/controls/CopyrightControl';
@@ -18,9 +24,29 @@ export default () => {
     apiKey: window.apiKey,
   });
 
+  const swissPolygon = new Polygon([
+    [
+      [1200512.3, 5740863.4],
+      [1200512.3, 6077033.16],
+      [656409.5, 6077033.16],
+      [656409.5, 5740863.4],
+      [1200512.3, 5740863.4],
+    ],
+  ]);
+
   const tracker = new TrajservLayer({
     url: 'https://api.geops.io/tracker/v1',
     apiKey: window.apiKey,
+    filterFunc: (f) => {
+      const { geometry, publisher } = f;
+      if (
+        swissPolygon.intersectsCoordinate(geometry.getFirstCoordinate()) ||
+        swissPolygon.intersectsCoordinate(geometry.getLastCoordinate())
+      ) {
+        return publisher === 'SBB';
+      }
+      return true;
+    },
   });
 
   tracker.onClick((vehicle) => {
@@ -28,6 +54,20 @@ export default () => {
     console.log(vehicle);
   });
 
+  const poly = new Feature({
+    geometry: swissPolygon,
+  });
+
+  const vectorSource1 = new Vector({
+    features: [poly],
+  });
+
+  const vectorLayer1 = new VectorLayer({
+    source: vectorSource1,
+  });
+
   map.addLayer(layer);
   map.addLayer(tracker);
+
+  map.addLayer(vectorLayer1);
 };
