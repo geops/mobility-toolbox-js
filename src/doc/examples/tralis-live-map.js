002612aa8703e181646dfdfa239e330a824871ac
edellucien <edellucien@gmail.com>
2021-03-23 14:01:29 +0100

    Use travic_v2 for new tralis example

--- src/doc/examples/tralis-live-map.js
+++ src/doc/examples/tralis-live-map.js
@@ -5,7 +5,7 @@ import LINE_IMAGES from './assets/tralis-live-map';
 export default () => {
   const map = new Map({
     container: 'map',
-    style: 'https://maps.geops.io/styles/travic/style.json',
+    style: 'https://maps.geops.io/styles/travic_v2/style.json',
     apiKey: window.apiKey,
     center: [11.55, 48.14],
     zoom: 10,
