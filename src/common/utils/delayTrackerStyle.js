a17142c379e7212ba594be8d869cb45a645f7071
Olivier Terral <olivier.terral@geops.de>
2021-11-26 17:21:21 +0100

    Use optimized version of the style

--- src/common/utils/delayTrackerStyle.js
+++ src/common/utils/delayTrackerStyle.js
@@ -8,6 +8,140 @@ import {
 } from '../trackerConfig';
 
 const styleCache = {};
+const cacheDelayBg = {};
+
+const createCanvas = (width, height) => {
+  const canvas = document.createElement('canvas');
+  canvas.width = width;
+  canvas.height = height;
+  return canvas;
+};
+
+// Draw circle delay background
+export const getDelayBgCanvas = (origin, radius, color) => {
+  const key = `${origin}, ${radius}, ${color}`;
+  if (!cacheDelayBg[key]) {
+    // console.log('cacheDelayBg');
+    const canvas = createCanvas(origin * 2, origin * 2);
+    const ctx = canvas.getContext('2d');
+    ctx.beginPath();
+    ctx.arc(origin, origin, radius, 0, 2 * Math.PI, false);
+    ctx.fillStyle = color;
+    ctx.filter = 'blur(1px)';
+    ctx.fill();
+    cacheDelayBg[key] = canvas;
+  }
+  return cacheDelayBg[key];
+};
+
+// Draw delay text
+const cacheDelayText = {};
+export const getDelayTextCanvas = (
+  width,
+  text,
+  fontSize,
+  font,
+  delayColor,
+  delayOutlineColor,
+  pixelRatio,
+) => {
+  const key = `${width}, ${text}, ${font}, ${delayColor}, ${delayOutlineColor}, ${pixelRatio}`;
+  if (!cacheDelayText[key]) {
+    const canvas = createCanvas(width, fontSize + 8);
+    const ctx = canvas.getContext('2d');
+    ctx.textAlign = 'left';
+    ctx.textBaseline = 'middle';
+    ctx.font = font;
+    ctx.fillStyle = delayColor;
+    ctx.strokeStyle = delayOutlineColor;
+    ctx.lineWidth = 1.5 * pixelRatio;
+    const delayText = text;
+    ctx.strokeText(delayText, 0, fontSize);
+    ctx.fillText(delayText, 0, fontSize);
+    cacheDelayText[key] = canvas;
+  }
+  return cacheDelayText[key];
+};
+
+// Draw colored circle with black border
+const cacheCircle = {};
+export const getCircleCanvas = (
+  origin,
+  radius,
+  color,
+  hasStroke,
+  hasDash,
+  pixelRatio,
+) => {
+  const key = `${origin}, ${radius}, ${color}, ${hasStroke},  ${hasDash}, ${pixelRatio}`;
+  if (!cacheCircle[key]) {
+    // console.log('cacheDelayBg');
+    const canvas = createCanvas(origin * 2, origin * 2);
+    const ctx = canvas.getContext('2d');
+    ctx.fillStyle = color;
+
+    if (hasStroke) {
+      ctx.lineWidth = 1 * pixelRatio;
+      ctx.strokeStyle = '#000000';
+    }
+
+    ctx.beginPath();
+    ctx.arc(origin, origin, radius, 0, 2 * Math.PI, false);
+    ctx.fill();
+
+    if (hasDash) {
+      ctx.setLineDash([5, 3]);
+    }
+
+    if (hasStroke) {
+      ctx.stroke();
+    }
+
+    cacheCircle[key] = canvas;
+  }
+  return cacheCircle[key];
+};
+
+// Draw text in the circle
+const cacheText = {};
+export const getTextCanvas = (
+  text,
+  origin,
+  textSize,
+  fillColor,
+  strokeColor,
+  hasStroke,
+  pixelRatio,
+) => {
+  const key = `${text}, ${origin}, ${textSize}, ${fillColor},${strokeColor}, ${hasStroke}, ${pixelRatio}`;
+  if (!cacheText[key]) {
+    const canvas = createCanvas(origin * 2, origin * 2);
+    const ctx = canvas.getContext('2d');
+
+    // Draw a stroke to the text only if a provider provides realtime but we don't use it.
+    if (hasStroke) {
+      ctx.save();
+      ctx.textBaseline = 'middle';
+      ctx.textAlign = 'center';
+      ctx.font = `bold ${textSize + 2}px Arial`;
+      ctx.strokeStyle = strokeColor;
+      ctx.strokeText(text, origin, origin);
+      ctx.restore();
+    }
+
+    // Draw a text
+    ctx.textBaseline = 'middle';
+    ctx.textAlign = 'center';
+    ctx.fillStyle = fillColor;
+    ctx.font = `bold ${textSize}px Arial`;
+    ctx.strokeStyle = strokeColor;
+    ctx.strokeText(text, origin, origin);
+    ctx.fillText(text, origin, origin);
+
+    cacheText[key] = canvas;
+  }
+  return cacheText[key];
+};
 
 /**
  * A tracker style that take in account the delay.
@@ -94,23 +228,22 @@ const style = (trajectory, viewState, options) => {
     const margin = 1 * pixelRatio;
     const radiusDelay = radius + 2;
     const markerSize = radius * 2;
+    const textWidth = 100;
+    const size = radiusDelay * 2 + margin * 2 + 100 * pixelRatio; // add space for delay information
+    const origin = size / 2;
 
-    const canvas = document.createElement('canvas');
-    // add space for delay information
-    canvas.width = radiusDelay * 2 + margin * 2 + 100 * pixelRatio;
-    canvas.height = radiusDelay * 2 + margin * 2 + 100 * pixelRatio;
+    // Create the canvas
+    const canvas = createCanvas(size, size);
     const ctx = canvas.getContext('2d');
-    const origin = canvas.width / 2;
 
     if (isDisplayStrokeAndDelay && delay !== null) {
       // Draw circle delay background
-      ctx.save();
-      ctx.beginPath();
-      ctx.arc(origin, origin, radiusDelay, 0, 2 * Math.PI, false);
-      ctx.fillStyle = getDelayColor(delay, cancelled);
-      ctx.filter = 'blur(1px)';
-      ctx.fill();
-      ctx.restore();
+      const delayBg = getDelayBgCanvas(
+        origin,
+        radiusDelay,
+        getDelayColor(delay, cancelled),
+      );
+      ctx.drawImage(delayBg, 0, 0);
     }
 
     // Show delay if feature is hovered or if delay is above 5mins.
@@ -119,21 +252,24 @@ const style = (trajectory, viewState, options) => {
       (hover || delay >= delayDisplay || cancelled)
     ) {
       // Draw delay text
-      ctx.save();
-      ctx.textAlign = 'left';
-      ctx.textBaseline = 'middle';
-      ctx.font = `bold ${Math.max(
+      const fontSize = Math.max(
         cancelled ? 19 : 14,
         Math.min(cancelled ? 19 : 17, radius * 1.2),
-      )}px arial, sans-serif`;
-      ctx.fillStyle = getDelayColor(delay, cancelled, true);
-
-      ctx.strokeStyle = delayOutlineColor;
-      ctx.lineWidth = 1.5 * pixelRatio;
-      const delayText = getDelayText(delay, cancelled);
-      ctx.strokeText(delayText, origin + radiusDelay + margin, origin);
-      ctx.fillText(delayText, origin + radiusDelay + margin, origin);
-      ctx.restore();
+      );
+      const delayText = getDelayTextCanvas(
+        textWidth,
+        getDelayText(delay, cancelled),
+        fontSize,
+        `bold ${fontSize}px arial, sans-serif`,
+        getDelayColor(delay, cancelled, true),
+        delayOutlineColor,
+        pixelRatio,
+      );
+      ctx.drawImage(
+        delayText,
+        origin + radiusDelay + margin,
+        origin - fontSize,
+      );
     }
 
     // Draw colored circle with black border
@@ -144,61 +280,46 @@ const style = (trajectory, viewState, options) => {
       circleFillColor = color || getBgColor(type);
     }
 
-    ctx.save();
-    if (isDisplayStrokeAndDelay || hover || selected) {
-      ctx.lineWidth = 1 * pixelRatio;
-      ctx.strokeStyle = '#000000';
-    }
-    ctx.fillStyle = circleFillColor;
-    ctx.beginPath();
-    ctx.arc(origin, origin, radius, 0, 2 * Math.PI, false);
-    ctx.fill();
-    // Dashed outline if a provider provides realtime but we don't use it.
-    if (
+    const hasStroke = isDisplayStrokeAndDelay || hover || selected;
+
+    const hasDash =
       isDisplayStrokeAndDelay &&
       useDelayStyle &&
       delay === null &&
-      operatorProvidesRealtime === 'yes'
-    ) {
-      ctx.setLineDash([5, 3]);
-    }
-    if (isDisplayStrokeAndDelay || hover || selected) {
-      ctx.stroke();
-    }
-    ctx.restore();
+      operatorProvidesRealtime === 'yes';
+
+    const circle = getCircleCanvas(
+      origin,
+      radius,
+      circleFillColor,
+      hasStroke,
+      hasDash,
+      pixelRatio,
+    );
+
+    ctx.drawImage(circle, 0, 0);
 
     // Draw text in the circle
     if (mustDrawText) {
-      const fontSize = Math.max(radius, 10 * pixelRatio);
+      const fontSize = Math.max(radius, 10);
       const textSize = getTextSize(ctx, markerSize, name, fontSize);
-
-      // Draw a stroke to the text only if a provider provides realtime but we don't use it.
-      if (
-        useDelayStyle &&
-        delay === null &&
-        operatorProvidesRealtime === 'yes'
-      ) {
-        ctx.save();
-        ctx.textBaseline = 'middle';
-        ctx.textAlign = 'center';
-        ctx.font = `bold ${textSize + 2}px Arial`;
-        ctx.strokeStyle = circleFillColor;
-        ctx.strokeText(name, origin, origin);
-        ctx.restore();
-      }
-
-      // Draw a text
-      ctx.save();
-      ctx.textBaseline = 'middle';
-      ctx.textAlign = 'center';
-      ctx.fillStyle = !useDelayStyle
+      const textColor2 = !useDelayStyle
         ? textColor || getTextColor(type)
         : '#000000';
-      ctx.font = `bold ${textSize}px Arial`;
-      ctx.strokeStyle = circleFillColor;
-      ctx.strokeText(name, origin, origin);
-      ctx.fillText(name, origin, origin);
-      ctx.restore();
+      const hasStroke2 =
+        useDelayStyle && delay === null && operatorProvidesRealtime === 'yes';
+
+      const text = getTextCanvas(
+        name,
+        origin,
+        textSize,
+        textColor2,
+        circleFillColor,
+        hasStroke2,
+        pixelRatio,
+      );
+
+      ctx.drawImage(text, 0, 0);
     }
 
     styleCache[key] = canvas;
