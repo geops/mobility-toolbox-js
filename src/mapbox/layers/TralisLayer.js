8726e54e62b89cccfed45a91d5ad3577e617768a
MateiAnton <anton.matei10@gmail.com>
2022-05-20 11:06:40 +0200

    wip

--- src/mapbox/layers/TralisLayer.js
+++ src/mapbox/layers/TralisLayer.js
@@ -91,6 +91,7 @@ class TralisLayer extends mixin(TrackerLayer) {
     this.api
       .getFullTrajectory(id, this.mode, this.generalizationLevel)
       .then((fullTrajectory) => {
+        console.log(JSON.stringify(fullTrajectory))
         this.map.getSource("selectedLineTraject").setData(fullTrajectory)
         console.log(this.map.getSource("selectedLineTraject"))
       })
