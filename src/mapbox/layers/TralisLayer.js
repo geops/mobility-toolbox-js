c5566a6bb89084bb892448bd74f73a0037aba7e9
MateiAnton <anton.matei10@gmail.com>
2022-05-23 13:51:49 +0200

    wip

--- src/mapbox/layers/TralisLayer.js
+++ src/mapbox/layers/TralisLayer.js
@@ -99,7 +99,10 @@ class TralisLayer extends mixin(TrackerLayer) {
             newCoords.push(toLonLat(coord))
           }
           element.coordinates = newCoords
+          fullTrajectory.features.push(element)
         });
+        fullTrajectory.features.shift()
+
         console.log(JSON.stringify(fullTrajectory))
         this.map.getSource("selectedLineTraject").setData(fullTrajectory)
         // console.log(this.map.getSource("selectedLineTraject"))
