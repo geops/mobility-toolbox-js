8237e7855fd9caa5810cf76ca0595937cb33f065
Olivier Terral <olivier.terral@geops.de>
2022-05-27 09:11:38 +0200

    Make sure document exist when creating the canvas

--- src/common/utils/simpleTrackerStyle.js
+++ src/common/utils/simpleTrackerStyle.js
@@ -2,17 +2,21 @@
  * A very simple tracker style.
  * Display blue point for each train.
  */
-const canvas = document.createElement('canvas');
-canvas.width = 15;
-canvas.height = 15;
-const ctx = canvas.getContext('2d');
-ctx.arc(8, 8, 5, 0, 2 * Math.PI, false);
-ctx.fillStyle = '#8ED6FF';
-ctx.fill();
-ctx.lineWidth = 3;
-ctx.strokeStyle = 'black';
-ctx.stroke();
-ctx.lineWidth = 3;
-
-const style = () => canvas;
+let canvas;
+const style = () => {
+  if (!canvas) {
+    document.createElement('canvas');
+    canvas.width = 15;
+    canvas.height = 15;
+    const ctx = canvas.getContext('2d');
+    ctx.arc(8, 8, 5, 0, 2 * Math.PI, false);
+    ctx.fillStyle = '#8ED6FF';
+    ctx.fill();
+    ctx.lineWidth = 3;
+    ctx.strokeStyle = 'black';
+    ctx.stroke();
+    ctx.lineWidth = 3;
+  }
+  return canvas;
+};
 export default style;
