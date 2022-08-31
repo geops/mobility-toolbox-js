a81d1a27604dc2c82a61a451e1ce51f5e94c88d9
Olivier Terral <olivier.terral@geops.com>
2022-08-31 11:55:50 +0200

    chore: fix remove event listener in realtime

--- src/ol/layers/RealtimeLayer.js
+++ src/ol/layers/RealtimeLayer.js
@@ -132,8 +132,8 @@ class RealtimeLayer extends mixin(Layer) {
     super.attachToMap(map);
     if (this.map) {
       this.olListenersKeys.push(
-        this.map.on(['moveend', 'change:target'], (evt) => {
-          const view = this.map.getView();
+        ...this.map.on(['moveend', 'change:target'], (evt) => {
+          const view = evt.map.getView();
           if (view.getAnimating() || view.getInteracting()) {
             return;
           }
