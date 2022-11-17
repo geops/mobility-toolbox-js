89b4a103135b415f5f61619041dfa400bb63345f
Olivier Terral <olivier.terral@geops.com>
2022-11-17 09:47:27 +0100

    chore: add separate config files

---
+++ .esdoc.js
@@ -0,0 +1,27 @@
+const config = {
+  source: './build',
+  destination: './apidoc',
+  includes: ['\\.(js)$', 'typedefs\\.js$'],
+  excludes: [
+    'setupTests\\.*',
+    '^iife',
+    '^mbt\\.*',
+    '\\.test\\.js$',
+    'doc/',
+    '(u|U)tils',
+  ],
+  plugins: [
+    {
+      name: 'esdoc-standard-plugin',
+    },
+    { name: 'esdoc-ecmascript-proposal-plugin', option: { all: true } },
+    {
+      name: 'esdoc-publish-html-plugin',
+    },
+    { name: './esdoc/plugins/dynamic-property-plugin/Plugin.js' },
+    { name: './esdoc/plugins/externals-plugin/Plugin.js' },
+    { name: './esdoc/plugins/optional-chaining-plugin/Plugin.js' },
+  ],
+};
+
+export default config;
---
+++ .stylelintrc.js
@@ -0,0 +1,6 @@
+const config = {
+  plugins: ['stylelint-scss'],
+  extends: ['stylelint-config-standard', 'stylelint-config-recommended-scss'],
+};
+
+export default config;
