4163d0472aa59a1584aa035cc90e084a66a38ab4
Olivier Terral <olivier.terral@geops.com>
2022-11-17 10:08:09 +0100

    chore: up libs

--- cypress/integration/examples/api.spec.js
+++
@@ -1,7 +0,0 @@
-describe('Api', () => {
-  it('loads api page', () => {
-    cy.visit('/doc');
-    cy.get('img[alt=Logo').should('be.ok');
-    cy.get('iframe').should('be.ok');
-  });
-});
--- cypress/integration/examples/examples.spec.js
+++
@@ -1,7 +0,0 @@
-describe('Examples', () => {
-  it('loads a map in the simple map example', () => {
-    cy.visit('/examples');
-    cy.get('img[alt=Logo').should('be.ok');
-    cy.get('canvas').should('be.ok');
-  });
-});
