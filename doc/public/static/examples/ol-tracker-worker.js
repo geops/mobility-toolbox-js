77633b6f887604ab2bf5fc48429838850d2b92ec
Olivier Terral <olivier.terral@geops.com>
2022-11-29 11:46:20 +0100

    chore: add exemple

---
+++ doc/public/static/examples/ol-tracker-worker.js
@@ -0,0 +1,46 @@
+import { Map, View } from 'ol';
+import {
+  RealtimeLayer,
+  MaplibreLayer,
+  CopyrightControl,
+} from 'mobility-toolbox-js/ol';
+import 'ol/ol.css';
+
+export default () => {
+  const map = new Map({
+    target: 'map',
+    view: new View({
+      center: [831634, 5933959],
+      zoom: 13,
+      minZoom: 5,
+    }),
+    controls: [],
+  });
+
+  const control = new CopyrightControl();
+  control.attachToMap(map);
+
+  const layer = new MaplibreLayer({
+    url: 'https://maps.geops.io/styles/travic_v2/style.json',
+    apiKey: window.apiKey,
+  });
+  layer.attachToMap(map);
+
+  const tracker = new RealtimeLayer({
+    url: 'wss://api.geops.io/tracker-ws/v1/',
+    apiKey: window.apiKey,
+    allowRenderWhenAnimating:
+      new URL(window.location.href)?.searchParams?.get(
+        'allowRenderWhenAnimating',
+      ) === 'true',
+    useWorker: true,
+  });
+  tracker.attachToMap(map);
+
+  tracker.onClick(([feature]) => {
+    if (feature) {
+      // eslint-disable-next-line no-console
+      console.log(feature.getProperties());
+    }
+  });
+};
