a65d2f746bfde8be2ca3ae09d855e5ece048ab5a
Olivier Terral <olivier.terral@geops.com>
2025-02-19 23:05:46 +0100

    fix: add ol and maplibregl to iife build

--- src/iife.js
+++ src/iife.js
@@ -1,7 +1,18 @@
-import mbt from './index';
+import maplibregl from 'maplibre-gl';
+import * as ol from 'ol';
+import * as control from 'ol/control';
+import * as interaction from 'ol/interaction';
+import * as layer from 'ol/layer';
+import * as source from 'ol/source';
 
+import mbt from './index';
 if (typeof window !== 'undefined') {
   window.mbt = mbt;
+  window.ol = ol;
+  window.ol.layer = layer;
+  window.ol.control = control;
+  window.ol.interaction = interaction;
+  window.ol.source = source;
+  window.maplibregl = maplibregl;
 }
-
 export default mbt;
