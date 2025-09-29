4bfefb70ac410199a97ea0e33474b255c8799bcb
danji90 <daniel.marsh-hunn@geops.com>
2025-09-29 14:49:16 +0200

    chore: adjust padding in mapset example

--- doc/public/static/examples/ol-mapset-layer.js
+++ doc/public/static/examples/ol-mapset-layer.js
@@ -11,27 +11,21 @@ export default () => {
   const mapsetLayer = new MapsetLayer({
     tenants: ['geopstest'],
     apiKey: window.apiKey,
-    api: new MapsetAPI({
-      apiKey: window.apiKey,
-    }),
     planId: 'dcb03ae5-9bef-4121-ba7b-3ad55e6d569e',
   });
 
   const map = new Map({
     layers: [baseLayer, mapsetLayer],
     target: 'map',
-    view: new View({
-      center: [950690.34, 6003962.67],
-      zoom: 20,
-    }),
   });
 
   mapsetLayer.once('updatefeatures', () => {
-    const features = mapsetLayer.getSource().getFeatures();
+    const source = mapsetLayer.getSource();
+    const features = source.getFeatures();
     if (features.length) {
-      map?.getView().fit(mapsetLayer?.getSource().getExtent(), {
+      map?.getView().fit(source.getExtent(), {
         duration: 500,
-        padding: [200, 200, 200, 200],
+        padding: [100, 100, 100, 100],
       });
     }
   });
