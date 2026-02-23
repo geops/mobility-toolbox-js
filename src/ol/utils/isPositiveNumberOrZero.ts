e7da7aff8da5570726657f0b515a8d7577fd1385
danji90 <daniel.marsh-hunn@geops.com>
2026-02-23 12:03:15 +0100

    chore: add documentation

--- src/ol/utils/isPositiveNumberOrZero.ts
+++ src/ol/utils/isPositiveNumberOrZero.ts
@@ -1,3 +1,9 @@
+/**
+ * This function checks if a value is a positive number or zero.
+ *
+ * @param {unknown} value - The value to check.
+ * @returns {boolean} - True if the value is a positive number or zero, false otherwise.
+ */
 function isPositiveNumberOrZero(value: unknown): value is number {
   return typeof value === 'number' && Number.isFinite(value) && value >= 0;
 }
