c70306023bc7f3caf9103e51fe697430cb2e99b4
Olivier Terral <olivier.terral@geops.com>
2026-02-25 09:34:41 +0100

    fix: fix types

--- src/types/realtime-asyncapi-types/GeometryType.ts
+++ src/types/realtime-asyncapi-types/GeometryType.ts
@@ -3,6 +3,7 @@ enum GeometryType {
   FEATURE_COLLECTION = 'FeatureCollection',
   GEOMETRY_COLLECTION = 'GeometryCollection',
   LINE_STRING = 'LineString',
+  MULTI_LINE_STRING = 'MultiLineString',
   MULTI_POINT = 'MultiPoint',
 }
 export default GeometryType;
